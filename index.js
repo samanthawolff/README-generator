bdf437cdbc62239adf02bab84bf1257ae8e16799
Samantha Wolff <samanthawolff07@gmail.com>
2022-03-31 15:05:22 -0500

    add description, installation, and usage questions

--- index.js
+++ index.js
@@ -17,6 +17,50 @@ const promptUser = () => {
                     return false;
                 };
             },
+        },
+        {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Enter a description of your project (Required):',
+            validate: projectDescriptionInput => {
+                if (projectDescriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description for your project!');
+                    return false;
+                };
+            },
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstallation',
+            message: 'Would you like to include installation instruction?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please enter the installation instructions:',
+            when: ({ confirmInstallation }) => {
+                if (confirmInstallation) {
+                    return true;
+                } else {
+                    return false;
+                };
+            },
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is this projects usage for? (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your projects usage!');
+                    return false;
+                };
+            },
         }
     ]);
 };
