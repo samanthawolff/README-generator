ecbe76a0e83a2be35b5f14ef8d73bd424c69dc25
Samantha Wolff <samanthawolff07@gmail.com>
2022-03-31 15:36:11 -0500

    add username, email, and reach out questions

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const promptUser = () => {
     return inquirer.prompt([
+
+        // Project name question
         {
             type: 'input',
             name: 'projectName',
-            message: 'What is your projects name? (Required)',
+            message: 'What is your projects name?',
             validate: projectNameInput => {
                 if (projectNameInput) {
                     return true;
@@ -18,10 +20,12 @@ const promptUser = () => {
                 };
             },
         },
+
+        // Project description question
         {
             type: 'input',
             name: 'projectDescription',
-            message: 'Enter a description of your project (Required):',
+            message: 'Enter a description of your project:',
             validate: projectDescriptionInput => {
                 if (projectDescriptionInput) {
                     return true;
@@ -31,6 +35,8 @@ const promptUser = () => {
                 };
             },
         },
+
+        // Installation and confirm installation questions
         {
             type: 'confirm',
             name: 'confirmInstallation',
@@ -49,10 +55,12 @@ const promptUser = () => {
                 };
             },
         },
+
+        // Project usage question
         {
             type: 'input',
             name: 'usage',
-            message: 'What is this projects usage for? (Required)',
+            message: 'What is this projects usage for?',
             validate: usageInput => {
                 if (usageInput) {
                     return true;
@@ -62,10 +70,12 @@ const promptUser = () => {
                 };
             },
         }, 
+
+        // Contributors question
         {
             type: 'input',
             name: 'contributors',
-            message: 'Please add any contributors (Required):',
+            message: 'Please add any contributors',
             validate: contributorsInput => {
                 if (contributorsInput) {
                     return true;
@@ -75,6 +85,8 @@ const promptUser = () => {
                 };
             },
         },
+
+        // Test instructions and confirm test instructions questions
         {
             type: 'confirm',
             name: 'confirmTestInstructions',
@@ -93,11 +105,58 @@ const promptUser = () => {
                 };
             },
         },
+
+        // License question
         {
             type: 'checkbox',
             name: 'license',
-            message: 'Please select the license for this project (Required):',
+            message: 'Please select the license for this project:',
             choices: ['Apache', 'Academic', 'GNU', 'ISC', 'MIT', 'Mozilla', 'Open']
+        },
+
+        // GitHub username question
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Enter your GitHub username:',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                };
+            },
+        },
+
+        // Email question
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your Email:',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Email!');
+                    return false;
+                };
+            },
+        },
+
+        // How to reach out question
+        {
+            type: 'input',
+            name: 'reachOut',
+            message: 'Please provide instructions on how to reach you:',
+            validate: reachOutInput => {
+                if (reachOutInput) {
+                    return true;
+                } else {
+                    console.log('Please provide instructions on how to reach you!');
+                    return false;
+                };
+            },
         }
     ]);
 };
